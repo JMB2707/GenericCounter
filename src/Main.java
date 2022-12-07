c53ba04ead04b5d51e17c147aac43a52accb8892
JMB2707 <Joshua.M.Banks@aexp.com>
2022-12-07 12:33:00 +0000

    Cleaned up some warnings

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Apple apple = new Apple();
-        Box<Apple> box = new Box<Apple>();
+        Box<Apple> box = new Box<>();
         Cart cart = new Cart();
-        Counter<Cart> counter = new Counter<Cart>();
+        Counter<Cart> counter = new Counter<>();
 
         box.add(apple);
         box.add(apple);//2 apples
-        cart.add(box);;
+        cart.add(box);
         cart.add(box);//2*2=4
         counter.add(cart);
         counter.add(cart);//4*2=8
